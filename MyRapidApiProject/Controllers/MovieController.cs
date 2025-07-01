4afd27890b7cea1ef02de33a71080df88b188ddc
Yasin Kereci <yke144907@stud.gibb.ch>
2025-07-01 13:04:31 +0200

    Add MovieController with action to fetch top 250 movies from RapidAPI

--- MyRapidApiProject/Controllers/MovieController.cs
+++ MyRapidApiProject/Controllers/MovieController.cs
@@ -4,8 +4,25 @@ namespace MyRapidApiProject.Controllers
 {
     public class MovieController : Controller
     {
-        public IActionResult MovieList()
+        public async Task<IActionResult> MovieList()
         {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://imdb236.p.rapidapi.com/api/imdb/top250-movies"),
+                Headers =
+    {
+        { "x-rapidapi-key", "254c94ea90msh20f1ab536df1b2cp1b0996jsn9b0a09805501" },
+        { "x-rapidapi-host", "imdb236.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(body);
+            }
             return View();
         }
     }
